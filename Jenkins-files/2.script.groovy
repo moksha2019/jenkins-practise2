fbac2060493948931216533d08b3ccaaea50ae59
moksha2019 <81618348+moksha2019@users.noreply.github.com>
2025-05-31 23:15:55 +0530

    added script block

---
+++ Jenkins-files/2.script.groovy
@@ -0,0 +1,20 @@
+// script  block comes  under 
+pipeline {
+    agent any
+    stages {
+        stage ("build phase:"){
+            steps {
+                echo "build completed  successfully: "
+                // we write script  block  unser the  steps  block  for writing the  groovy code
+                script {
+                    def  course = "k8s"
+                    if  (course == "k8s") {
+                        println("Thanks  for enrolling  into $course")
+                    }
+                    else
+                     println ("Do learn  K8s")
+                }
+            }
+        }
+    }
+}
