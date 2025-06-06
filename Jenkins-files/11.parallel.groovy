2b90d93ebca16fe3ee9dd27d8e332b877afb5555
moksha2019 <81618348+moksha2019@users.noreply.github.com>
2025-06-07 00:24:12 +0530

    will  continute  further  later

---
+++ Jenkins-files/11.parallel.groovy
@@ -0,0 +1,16 @@
+// sometime we need to run the  jobs parallely 
+// Stages in Declarative Pipeline may have a parallel section containing a list of nested stages to be run in parallel
+
+pipeline {
+    agent any
+    stages {
+        stage ('Build stage') {
+            parallel {
+                stage ('stage1')
+            }
+            steps {
+                echo "Build completed: "
+            }
+        }
+    }
+}
